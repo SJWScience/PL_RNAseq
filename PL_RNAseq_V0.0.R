1ee09d129a67301b38205cbab175b0d25f6f3a4d
SJWScience <s.taylor_wardell@hotmail.com>
2021-04-20 14:06:32 +1200

    working update

--- PL_RNAseq_V0.0.R
+++ PL_RNAseq_V0.0.R
@@ -17,7 +17,7 @@ library(DESeq2)
 library(apeglm)
 library(pheatmap)
 
-input_star <- DESeqDataSetFromHTSeqCount(sampleTable = input_table_DESEQ2, directory = (args[4]), design = Gene ~ Condition)
+input_star <- DESeqDataSetFromHTSeqCount(sampleTable = input_table_DESEQ2, directory = (args[4]), design =  ~ Condition + Gene)
 
 nrow(input_star)
 
@@ -36,6 +36,35 @@ write.table(DESEQ2_norm_counts, "DESEQ2_norm_counts.txt", quote=F, col.names=T, 
 resultsNames(input_star2)
 
 DESEQ2_DEG <- results(object = input_star2, name="Condition_Un_vs_In")
+DESEQ2_DEG_alt <- results(object = input_star2, name="Gene_spoT_vs_relA")
 ##this part needs to not be hardcoded##
 
 head(DESEQ2_DEG)
+head(DESEQ2_DEG_alt)
+
+DESEQ2_DEG_shrink <- lfcShrink(dds = input_star2, coef="Condition_Un_vs_In", type="apeglm")
+head(DESEQ2_DEG_shrink)
+
+DESEQ2_alt_DEG_shrink <- lfcShrink(dds = input_star2, coef="Gene_spoT_vs_relA", type="apeglm")
+head(DESEQ2_alt_DEG_shrink)
+
+write.table(DESEQ2_DEG_shrink, "../DESEQ2_DEG_shrink.txt", quote=F, col.names=T, row.names=T, sep="\t")
+write.table(DESEQ2_alt_DEG_shrink, "../DESEQ2_alt_DEG_shrink.txt", quote=F, col.names=T, row.names=T, sep="\t")
+
+write.table(DESEQ2_DEG, "../DESEQ2_DEG.txt", quote=F, col.names=T, row.names=T, sep="\t")
+write.table(DESEQ2_DEG_alt, "../DESEQ2_DEG_alt.txt", quote=F, col.names=T, row.names=T, sep="\t")
+
+DESEQ2_var_stabl <- vst(input_star2)
+
+DESEQ2_DistMatrix <- as.matrix(dist(t(assay(DESEQ2_var_stabl))))
+
+png("sample_distance_heatmap_DESEQ.png")
+pheatmap(DESEQ2_DistMatrix)
+
+png("DESEQ_PCA.png")
+plotPCA(object = DESEQ2_var_stabl, intgroup = "Condition")
+
+png("DESEQ_gene_PCA.png")
+plotPCA(object = DESEQ2_var_stabl, intgroup = "Gene")
+
+dev.off()
